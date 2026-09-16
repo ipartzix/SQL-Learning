ca99957972bdf4f4db61a6f3df7c0b5f643385a7
Partha Paul <parthapaul2705@gmail.com>
2026-09-16 21:06:26 +0530

    feat(sql):do different alter   operations

---
+++ Learn_PostgreSQL/constraint_check .sql
@@ -0,0 +1,18 @@
+-- Constraint check
+select * from employees;
+
+alter table employees drop ph_number;
+
+alter table  employees add column mobile_no varchar(20) unique check ( length(mobile_no)= 10);
+
+
+insert  into employees values(11, 'raghu', 'Sharma', 'work.raghu101@gmail.com', 'IT', 50000.00, '2020-01-15',23,1234567839);
+insert  into employees values(12, 'ranu', 'Sharma', 'isklem@gmail.com', 'IT', 50000.00, '2020-01-15',23,1286804999);
+
+--  Name Constraint
+alter table employees add constraint  email_at_list_5_words check ( length(email)>=5 );
+
+
+insert  into employees values(13, 'ritu', 'Sharma', 'riyu.work@gmail.com', 'IT', 54100.00, '2020-01-15',31,1245678391);
+
+insert into  employees (emp_id, fname, lname, email) values (14,'Kushal','Das','kusal@gmail.com');

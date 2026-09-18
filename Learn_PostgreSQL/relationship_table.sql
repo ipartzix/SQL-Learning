ea2c5764fdfcf898f426523f7e5d88857defd19b
Partha Paul <parthapaul2705@gmail.com>
2026-09-18 08:43:39 +0530

    feat(sql): create table for customers and order with foreign key

--- Learn_PostgreSQL/relationship_table.sql
+++ Learn_PostgreSQL/relationship_table.sql
@@ -2,3 +2,21 @@
 -- One  to One
 -- One to Many
 -- Many to Many
+
+create database store_db;
+
+--
+CREATE TABLE customers
+(
+    cust_id   SERIAL PRIMARY KEY,
+    cust_name VARCHAR(100) NOT NULL
+);
+
+CREATE  TABLE orders(
+    ord_id SERIAL PRIMARY KEY ,
+    order_date DATE NOT NULL ,
+    price NUMERIC NOT NULL ,
+
+    cust_id INTEGER NOT NULL ,
+    FOREIGN KEY(cust_id) REFERENCES customers(cust_id)
+);

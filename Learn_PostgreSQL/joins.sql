2d7dad20820aeee7dd2b0bcb091e88674f5da02b
Partha Paul <parthapaul2705@gmail.com>
2026-09-18 12:11:39 +0530

    feat(sql):implement inner join operation with count and sum

--- Learn_PostgreSQL/joins.sql
+++ Learn_PostgreSQL/joins.sql
@@ -4,8 +4,35 @@
 -- 2.Inner Join
 -- 3.Left Join
 -- 4.Right Join
+
 select * from orders;
 select * from customers;
 
 -- CROSS JOIN
+-- Every row from one table is combined with every row from another table.
+
 SELECT * from customers cross join orders ;
+
+-- INNER JOIN
+-- Returns only the rows where there is a match
+-- between the specified columns in both the
+-- left (or first) and right (or second) tables.
+
+SELECT * from
+customers
+inner join
+orders
+on customers.cust_id = orders.cust_id ;
+-- we can also create alias for this like customers as client.cust_id == customers.cust_id
+
+
+SELECT c.cust_name, COUNT(o.ord_id) AS total_orders
+FROM customers as c
+INNER JOIN orders as o ON c.cust_id = o.cust_id
+GROUP BY c.cust_name;
+
+
+SELECT c.cust_name, sum(o.price) AS total_orders
+FROM customers as c
+INNER JOIN orders as o ON c.cust_id = o.cust_id
+GROUP BY c.cust_name;

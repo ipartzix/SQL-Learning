28dd2f78831bc0174bd0f6957765f08a99ae512d
Partha Paul <parthapaul2705@gmail.com>
2026-09-18 10:14:20 +0530

    feat(sql):implement cross join

---
+++ Learn_PostgreSQL/joins.sql
@@ -0,0 +1,11 @@
+-- Types of Join
+
+-- 1.Cross Join
+-- 2.Inner Join
+-- 3.Left Join
+-- 4.Right Join
+select * from orders;
+select * from customers;
+
+-- CROSS JOIN
+SELECT * from customers cross join orders ;
